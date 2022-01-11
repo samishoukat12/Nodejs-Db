ae40ef609d99b0dd8740a5ef9ac86467385dc10c
Muhammad Sami Shoukat <88966319+samishoukat12@users.noreply.github.com>
2022-01-11 20:31:07 +0500

    node

---
+++ Routes/auth.js
@@ -0,0 +1,42 @@
+
+const authRoutes = require('express').Router();
+const User = require('../model/User')
+
+authRoutes.post('/register', (req, res) => {
+    res.send('register');
+})
+
+authRoutes.post('/getRegistered', async(req, res) => {
+   
+    try {
+      
+      if(req.body?.name || req.body?.email || req.body?.password){
+           const user = new User({
+            name:req.body.name,
+            email:req.body.email,
+            password:req.body.password
+        })
+        user.save((err,data)=>{
+            console.log('error',err);
+            console.log("data",data);
+        })
+       res.send(user)
+        console.log("users",user);
+        // const savedUser = await user.save()
+        // res.send(savedUser)
+          console.log("name",req.body?.name);
+          console.log("email",req.body?.email);
+          console.log("password",req.body?.password);
+      }
+      else{
+          console.log("error");
+      }
+       
+    } catch (error) {
+      console.log('not working');
+    }
+   
+})
+
+
+module.exports = authRoutes;
---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+var cors = require("cors");
+var bodyParser = require("body-parser")
+const mongoose = require('mongoose')
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static('public'))
+dotenv.config();
+//import Routes
+const authRoutes = require('./Routes/auth')
+//Routes Middleware
+app.use('/app/users', authRoutes);
+//DB
+mongoose.connect(process.env.DB_CONNECT, () => {
+
+        console.log('connect to db')
+  
+    
+})
+
+//middleware
+app.use(express.json())
+
+app.listen(3000, () => {
+    console.log('server is running');
+})
