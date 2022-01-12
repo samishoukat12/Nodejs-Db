469db255598c8dbd9c2060b03695546963c22c8b
Muhammad Sami Shoukat <88966319+samishoukat12@users.noreply.github.com>
2022-01-12 19:37:13 +0500

    code review

--- Routes/auth.js
+++ Routes/auth.js
@@ -3,39 +3,37 @@ const authRoutes = require('express').Router();
 const User = require('../model/User')
 
 authRoutes.post('/register', (req, res) => {
-    res.send('register');
+  res.send('register');
 })
 
-authRoutes.post('/getRegistered', async(req, res) => {
-   
-    try {
-      
-      if(req.body?.name || req.body?.email || req.body?.password){
-           const user = new User({
-            name:req.body.name,
-            email:req.body.email,
-            password:req.body.password
-        })
-        user.save((err,data)=>{
-            console.log('error',err);
-            console.log("data",data);
-        })
-       res.send(user)
-        console.log("users",user);
-        // const savedUser = await user.save()
-        // res.send(savedUser)
-          console.log("name",req.body?.name);
-          console.log("email",req.body?.email);
-          console.log("password",req.body?.password);
-      }
-      else{
-          console.log("error");
-      }
-       
-    } catch (error) {
-      console.log('not working');
+authRoutes.post('/getRegistered', async (req, res) => {
+
+  try {
+
+    if (req.body?.name || req.body?.email || req.body?.password) {
+      const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      })
+      user.save((err, data) => {
+        console.log('error', err);
+        console.log("data", data);
+      })
+      res.send(user)
+      console.log("users", user);
+      console.log("name", req.body?.name);
+      console.log("email", req.body?.email);
+      console.log("password", req.body?.password);
     }
-   
+    else {
+      console.log("error");
+    }
+
+  } catch (error) {
+    console.log('not working');
+  }
+
 })
 
 
--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const app = express()
-// const dotenv = require('dotenv')
 var cors = require("cors");
 var bodyParser = require("body-parser")
-// const mongoose = require('mongoose')
 require("dotenv").config({ path:"./.env" });
-const Db=require('./db/Db')
+const Database=require('./db/Db')
 app.use(
     bodyParser.urlencoded({
         extended: true,
@@ -14,21 +12,12 @@ app.use(
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('public'))
-// dotenv.config();
-
 //import Routes
 const authRoutes = require('./Routes/auth')
 //Routes Middleware
 app.use('/app/users', authRoutes);
-//DB
-// mongoose.connect(process.env.DB_CONNECT, () => {
-
-//         console.log('connect to db')
-  
-    
-// })
-
-Db();
+//Db
+Database();
 //middleware
 app.use(express.json())
 
