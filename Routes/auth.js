6f34825800c0151ee7d3f132d63b623ad0ed9a98
Muhammad Sami Shoukat <88966319+samishoukat12@users.noreply.github.com>
2022-01-12 23:52:29 +0500

    validation

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,36 +1,52 @@
 
 const authRoutes = require('express').Router();
 const User = require('../model/User')
-
+// const Joi = require('joi');
 authRoutes.post('/register', (req, res) => {
   res.send('register');
 })
-
-authRoutes.post('/getRegistered', async (req, res) => {
-
+//validation
+// const Schemaa=Joi.object({
+//   name:Joi
+//   .string()
+//   .required(),
+//   email:Joi
+//   .string()
+//   .lowercase()
+//   .min(3)
+//   .required()
+//   .email(),
+//   password:Joi
+//   .string()
+//   .min(5)
+//   .required()
+// })
+authRoutes.post('/getRegistered', async(req, res) => {
+  
   try {
+    // const validation =await Joi.validateAsync(req?.body,Schemaa)
+    // console.log(validation,Schemaa);
+  if (req.body?.name || req.body?.email || req.body?.password) {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    })
+    user.save((err, data) => {
+      console.log('error', err);
+      console.log("data", data);
+    })
+    res.send(user)
+    console.log("users", user);
+    console.log("name", req.body?.name);
+    console.log("email", req.body?.email);
+    console.log("password", req.body?.password);
+  }
+  else {
+    console.log("error");
+  }
 
-    if (req.body?.name || req.body?.email || req.body?.password) {
-      const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-      })
-      user.save((err, data) => {
-        console.log('error', err);
-        console.log("data", data);
-      })
-      res.send(user)
-      console.log("users", user);
-      console.log("name", req.body?.name);
-      console.log("email", req.body?.email);
-      console.log("password", req.body?.password);
-    }
-    else {
-      console.log("error");
-    }
-
-  } catch (error) {
+  } catch (err) {
     console.log('not working');
   }
 
